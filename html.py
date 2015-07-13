4d0a5a310cf1f3c3d8f04895da7b2485ca05fde0
brockuniera <brockuniera@utexas.edu>
2015-07-13 09:46:12 -0500

    Update gitignore, minor testing with html.py

--- html.py
+++ html.py
@@ -2,9 +2,15 @@ import HTMLParser
 import requests
 
 
+def get_webtext(url="https://en.wikipedia.org/wiki/Deus_Ex"):
+    page = requests.get(url)
+    return page.text
+
+
 class WikiParse(HTMLParser.HTMLParser):
     def handle_starttag(self, tag, attrs):
-        print "start tag", tag
+        if tag == 'p':
+            print "start tag", tag
 
     def handle_endtag(self, tag):
         print "end tag", tag
@@ -14,4 +20,11 @@ class WikiParse(HTMLParser.HTMLParser):
 
 
 if __name__ == "__main__":
+    # GOAL: Read wikipedia.org/philosophy to user
+
+    # Get a webpage
+    webtext = get_webtext()
+
+    # Build our parser
     parser = WikiParse()
+    parser.feed("<html><body><p>asdf</p></html></body>")
