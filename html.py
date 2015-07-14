79e1cdd566b13a7b3753980c992df85d8608ff42
brockuniera <brockuniera@utexas.edu>
2015-07-14 09:49:35 -0500

    Can sort of parse wikipedia now

--- html.py
+++ html.py
@@ -1,4 +1,4 @@
-import HTMLParser
+from HTMLParser import HTMLParser
 import requests
 
 
@@ -7,16 +7,40 @@ def get_webtext(url="https://en.wikipedia.org/wiki/Deus_Ex"):
     return page.text
 
 
-class WikiParse(HTMLParser.HTMLParser):
+class WikiParse(HTMLParser):
+
+    def __init__(self):
+        """
+        Initialize variables to keep track of state
+        """
+        HTMLParser.__init__(self)
+        self.allow_printing = False
+        self.depth = 0
+
     def handle_starttag(self, tag, attrs):
-        if tag == 'p':
-            print "start tag", tag
+        # Find the div we care about; it has id="mw-content-text"
+        if not self.allow_printing:
+            for tup in attrs:
+                if 'id' == tup[0] and 'mw-content-text' in tup[1:]:
+                    self.allow_printing = True
+                    break
+
+        # Once we do, start printing data and keeping track of depth
+        if self.allow_printing:
+            self.depth += 1
 
     def handle_endtag(self, tag):
-        print "end tag", tag
+        # If we encounter an end tag with the same depth as
+        # our starting div, we should stop!
+        if self.allow_printing:
+            self.depth -= 1
+            if self.depth == 0:
+                self.allow_printing = False
 
     def handle_data(self, data):
-        print "data", data
+        # Once we find our div, start printing everything
+        if self.allow_printing:
+            print data
 
 
 if __name__ == "__main__":
@@ -27,4 +51,4 @@ if __name__ == "__main__":
 
     # Build our parser
     parser = WikiParse()
-    parser.feed("<html><body><p>asdf</p></html></body>")
+    parser.feed(webtext)
